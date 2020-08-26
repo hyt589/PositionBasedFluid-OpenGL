715b09174ff629313af9db19d14a1bc3542225a2
hyt <yxh589@case.edu>
2020-08-26 23:27:36 +0800

    setting up dependencies; setting up rendering window

---
+++ src/global.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <imgui.h>
+#include <glm.hpp>
+#include <json.hpp>
+#include <fstream>
+
+using JSON = nlohmann::json;
+
+#define LOG_INFO(msg) std::cout << "[Info] " << msg << std::endl;
+#define LOG_ERR(msg) std::cerr << "[Error] " << msg << std::endl;
+#define LOG_WARN(msg) std::cout << "[Warning] " << msg << std::endl;
+
+namespace global{
+    JSON config;
+    bool config_loaded = false;
+
+    void load_config(std::string path){
+        std::ifstream in(path);
+        in >> config;
+        config_loaded = true;
+    }
+} 
+
+auto & CONFIG = global::config;
---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include "global.hpp"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main(int argc, char** argv) {
+    
+    if(argc < 2){
+        global::load_config("config.json");
+    } else {
+        global::load_config(argv[1]);
+    }
+
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, CONFIG["renderer"]["glVersion"]["major"]);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, CONFIG["renderer"]["glVersion"]["minor"]);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    std::string title = CONFIG["renderer"]["windowTitle"];
+
+    GLFWwindow * window = glfwCreateWindow(CONFIG["renderer"]["resolution"]["width"],
+                                        CONFIG["renderer"]["resolution"]["height"], 
+                                        title.c_str(), 
+                                        NULL, NULL);
+
+    if(window == NULL){
+        LOG_ERR("Failed to create GLFW window. Exiting...")
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        LOG_ERR("Failed to initialize GLAD")
+        return -1;
+    }    
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
