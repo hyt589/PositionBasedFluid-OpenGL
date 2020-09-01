dbabde6bd1622822607de6cc2cedf0f877cf50dc
hyt <yxh589@case.edu>
2020-09-01 21:53:43 +0800

    adjusted light intensity; cleaned up some code

--- src/app.cpp
+++ src/app.cpp
@@ -1,99 +1,5 @@
 #include "app.hpp"
 
-// unsigned int sphereVAO = 0;
-// unsigned int indexCount;
-// void renderSphere()
-// {
-//     if (sphereVAO == 0)
-//     {
-//         glGenVertexArrays(1, &sphereVAO);
-
-//         unsigned int vbo, ebo;
-//         glGenBuffers(1, &vbo);
-//         glGenBuffers(1, &ebo);
-
-//         std::vector<glm::vec3> positions;
-//         std::vector<glm::vec2> uv;
-//         std::vector<glm::vec3> normals;
-//         std::vector<unsigned int> indices;
-
-//         const unsigned int X_SEGMENTS = 64;
-//         const unsigned int Y_SEGMENTS = 64;
-//         const float PI = 3.14159265359;
-//         for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
-//         {
-//             for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
-//             {
-//                 float xSegment = (float)x / (float)X_SEGMENTS;
-//                 float ySegment = (float)y / (float)Y_SEGMENTS;
-//                 float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
-//                 float yPos = std::cos(ySegment * PI);
-//                 float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
-
-//                 positions.push_back(glm::vec3(xPos, yPos, zPos));
-//                 uv.push_back(glm::vec2(xSegment, ySegment));
-//                 normals.push_back(glm::vec3(xPos, yPos, zPos));
-//             }
-//         }
-
-//         bool oddRow = false;
-//         for (unsigned int y = 0; y < Y_SEGMENTS; ++y)
-//         {
-//             if (!oddRow) // even rows: y == 0, y == 2; and so on
-//             {
-//                 for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
-//                 {
-//                     indices.push_back(y       * (X_SEGMENTS + 1) + x);
-//                     indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
-//                 }
-//             }
-//             else
-//             {
-//                 for (int x = X_SEGMENTS; x >= 0; --x)
-//                 {
-//                     indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
-//                     indices.push_back(y       * (X_SEGMENTS + 1) + x);
-//                 }
-//             }
-//             oddRow = !oddRow;
-//         }
-//         indexCount = indices.size();
-
-//         std::vector<float> data;
-//         for (std::size_t i = 0; i < positions.size(); ++i)
-//         {
-//             data.push_back(positions[i].x);
-//             data.push_back(positions[i].y);
-//             data.push_back(positions[i].z);
-//             if (uv.size() > 0)
-//             {
-//                 data.push_back(uv[i].x);
-//                 data.push_back(uv[i].y);
-//             }
-//             if (normals.size() > 0)
-//             {
-//                 data.push_back(normals[i].x);
-//                 data.push_back(normals[i].y);
-//                 data.push_back(normals[i].z);
-//             }
-//         }
-//         glBindVertexArray(sphereVAO);
-//         glBindBuffer(GL_ARRAY_BUFFER, vbo);
-//         glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
-//         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-//         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-//         float stride = (3 + 2 + 3) * sizeof(float);
-//         glEnableVertexAttribArray(0);
-//         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-//         glEnableVertexAttribArray(1);
-//         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
-//         glEnableVertexAttribArray(2);
-//         glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
-//     }
-
-//     glBindVertexArray(sphereVAO);
-//     glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
-// }
 
 void processInput(GLFWwindow *window)
 {
@@ -167,7 +73,7 @@ void Application::run()
     program = new Program(shaders);
 
     cam = new Camera(
-        glm::vec3(0.f),
+        glm::vec3(0.f, 100.0f, 0.f),
         glm::vec3(1.f, 0.f, 0.f),
         glm::vec3(0.f, 1.f, 0.f)
     );
@@ -195,10 +101,10 @@ void Application::run()
 void Application::renderLoop()
 {
     glm::vec3 lightPositions[] = {
-        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(10.0f, 1200.0f, 10.0f),
     };
     glm::vec3 lightColors[] = {
-        glm::vec3(150.0f, 150.0f, 150.0f),
+        glm::vec3(1500000.0f, 1500000.0f, 1500000.0f),
     };
 
     GL(glEnable(GL_DEPTH_TEST));
